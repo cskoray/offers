eb93f9ad3fabcc1629e17030f0a6dea729bf367e
koray <k.kaya@hymnai.com>
2024-04-24 08:01:21 +0100

    yml & service fixes

--- src/main/java/com/solidcode/offers/service/OfferService.java
+++ src/main/java/com/solidcode/offers/service/OfferService.java
@@ -20,6 +20,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -87,6 +88,7 @@ public class OfferService {
         });
     }
 
+    @Transactional
     public void deleteFavorite(final String offerKey) {
 
         Offer offer = offerRepository.findByOfferKey(offerKey).orElseThrow(() -> new OfferNotFoundException(OFFER_NOT_FOUND, "key"));
