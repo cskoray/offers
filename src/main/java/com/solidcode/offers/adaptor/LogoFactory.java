81191e288876ddfb803031435659e2a9a3959ea5
koray <k.kaya@hymnai.com>
2024-04-25 21:53:08 +0100

    spring profile add

--- src/main/java/com/solidcode/offers/adaptor/LogoFactory.java
+++ src/main/java/com/solidcode/offers/adaptor/LogoFactory.java
@@ -3,10 +3,12 @@ package com.solidcode.offers.adaptor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class LogoFactory {
 
@@ -18,6 +20,8 @@ public class LogoFactory {
 
     public String getLogo(String merchantName) {
 
+        System.out.println("----------->LogoFactory: getLogo merchant name: " + merchantName + " clientId: " + clientId);
+        log.info("----------->LogoFactory: getLogo merchant name: {}, client_id {}", merchantName, clientId);
         String result = unsplashClient.searchPhotos(merchantName, clientId);
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = null;
